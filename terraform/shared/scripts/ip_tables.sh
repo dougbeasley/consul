be2bf3c89abd1e1a362c69e1e0f1373321cd8ee8
Doug Beasley <beasley.doug@gmail.com>
2017-01-04 13:36:37 -0800

    Add DNS forwarding rules

--- terraform/shared/scripts/ip_tables.sh
+++ terraform/shared/scripts/ip_tables.sh
@@ -6,6 +6,11 @@ sudo iptables -I INPUT -s 0/0 -p tcp --dport 8301 -j ACCEPT
 sudo iptables -I INPUT -s 0/0 -p tcp --dport 8302 -j ACCEPT
 sudo iptables -I INPUT -s 0/0 -p tcp --dport 8400 -j ACCEPT
 
+#DNS forwarding
+sudo iptables -I INPUT -s 0/0 -p tcp --dport 53 -j ACCEPT
+sudo iptables -t nat -I PREROUTING -p tcp --dport 53 -j REDIRECT --to-ports 8600
+sudo iptables -t nat -I OUTPUT -p tcp -o lo --dport 53 -j REDIRECT --to-ports 8600
+
 if [ -d /etc/sysconfig ]; then
   sudo iptables-save | sudo tee /etc/sysconfig/iptables
 else
