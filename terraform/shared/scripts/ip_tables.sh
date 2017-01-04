3b54dc1e1d4ce45f16f1280e5e3df1db3407989e
Doug Beasley <beasley.doug@gmail.com>
2017-01-04 15:07:26 -0800

    Adding UDP protocol

--- terraform/shared/scripts/ip_tables.sh
+++ terraform/shared/scripts/ip_tables.sh
@@ -9,7 +9,9 @@ sudo iptables -I INPUT -s 0/0 -p tcp --dport 8400 -j ACCEPT
 #DNS forwarding
 sudo iptables -I INPUT -s 0/0 -p tcp --dport 53 -j ACCEPT
 sudo iptables -t nat -I PREROUTING -p tcp --dport 53 -j REDIRECT --to-ports 8600
+sudo iptables -t nat -I PREROUTING -p udp --dport 53 -j REDIRECT --to-ports 8600
 sudo iptables -t nat -I OUTPUT -p tcp -o lo --dport 53 -j REDIRECT --to-ports 8600
+sudo iptables -t nat -I OUTPUT -p udp -o lo --dport 53 -j REDIRECT --to-ports 8600
 
 if [ -d /etc/sysconfig ]; then
   sudo iptables-save | sudo tee /etc/sysconfig/iptables
